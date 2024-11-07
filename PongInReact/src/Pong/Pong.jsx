4b4d5a863613fe0f5100313d0727b81a5ef4f6c4
Manoug Pfennig <mapfenni@c4r1p1.42nice.fr>
2024-11-07 18:16:25 +0100

    Added a React front-end version of Pong

--- PongInReact/src/Pong/Pong.jsx
+++ PongInReact/src/Pong/Pong.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import styles from './Pong.module.css';
 
 function Pong() {
+
     const canvasRef = useRef(null);
 	const keys = useRef({ lu: false, ld: false, ru: false, rd: false});
 	const LPaddle = useRef({ x: 50, y: 250});
@@ -16,12 +17,15 @@ function Pong() {
     const lastUpdateTimeRef = useRef(0);
     const [count, setCount] = useState(0);
 
-    document.body.style.backgroundColor = 'black';
+	useEffect(() => {
+		document.title = "Pong :)"; // Set the tab name here
+	}, []);
 
     // Counter effect for seconds since start
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCount((prevCount) => prevCount + 1);
+			if (gameStarted.current == true)
+	            setCount((prevCount) => prevCount + 1);
         }, 1000);
 
         return () => clearInterval(intervalId);
@@ -29,6 +33,7 @@ function Pong() {
 
 	useEffect(() => {
 
+		// Listens for KeyDown event
 		const handleKeyDown = (event) => {
 			switch (event.key)
 			{
@@ -46,6 +51,8 @@ function Pong() {
 					break;
 			}
 		};
+
+		// Listens for KeyUp event
 		const handleKeyUp = (event) => {
 			switch (event.key)
 			{
@@ -66,14 +73,16 @@ function Pong() {
 		window.addEventListener('keydown', handleKeyDown);
 		window.addEventListener('keyup', handleKeyUp);
 
-			return () => {
-				window.removeEventListener('keydown', handleKeyDown);
-				window.addEventListener('keyup', handleKeyUp);
-			};
-		}, []);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+			window.addEventListener('keyup', handleKeyUp);
+		};
+	}, []);
 
 	const handlePaddlesMovement = () =>
 	{
+		// Moves the paddles in the corresponding direction depending on pressed keys
+		// See handleKeyUp() and handleKeyDown() above
 		if (keys.current.lu)
 			LPaddle.current.y += (LPaddle.current.y <= 60 ? 0 : -5);
 		if (keys.current.ld)
@@ -93,10 +102,14 @@ function Pong() {
     };
 
 	const playAgain = () => {
+
+		// Adding score depending on the position of the ball, x=791.1 would be the right side, x=9 for left
 		if (pos.current.x == 791.1)
 			setScore((s) => s = {...s, left: s.left + 1});
 		else
 			setScore((s) => s = {...s, right: s.right + 1});
+
+		// Resets the gamestate to keep playing (except scores)
 		speed.current = 2;
 		pos.current = ({ x: 400, y: 250 });
 		obj.current = ({ x: 400, y: 250 });
@@ -107,6 +120,8 @@ function Pong() {
 	}
 
 	const restartGame = () => {
+
+		// Resets completely the gamestate
 		gameStarted.current = false;
 		pos.current = ({ x: 400, y: 250 });
 		obj.current = ({ x: 400, y: 250 });
@@ -119,7 +134,9 @@ function Pong() {
 	const startGame = () => {
 
 		if (gameStarted.current == true)
-			return ;
+		return ;
+		// Sets the gamestate to default values and allows the game to start moving the ball
+		setCount(0);
 		gameStarted.current = true;
 		setScore({ left: 0, right: 0 });
 		vec.current = 0.005;
@@ -129,6 +146,7 @@ function Pong() {
 	}
 
 	const drawBall = (ctx, x, y) => {
+		// Drawing the ball at the given position
 		ctx.beginPath();
 		ctx.arc(x, y, 10, 0, 2 * Math.PI);
 		ctx.fillStyle = 'white';
@@ -136,6 +154,7 @@ function Pong() {
 	};
 
 	const drawPaddle = (ctx, x, y) => {
+		// Drawing a paddle centered at the given position
 		ctx.beginPath();
 		ctx.rect(x, y - 60, 10, 120);
 		ctx.fillStyle = 'white';
@@ -144,18 +163,19 @@ function Pong() {
 
 	const drawGame = (ctx) =>
 	{
-		// Fill background with black
+		// Fill background in black
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 		ctx.fillStyle = 'black';
 		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-		// Draw center lines
+		// Drawing center lines for esthetics (looks nice, right ?)
 		ctx.beginPath();
 		for (let i = 0; i != 500; i += 10)
 			ctx.rect(398, i, 4, 1);
 		ctx.fillStyle = 'grey';
 		ctx.fill();
 
+		// Drawing non-static game elements
 		drawPaddle(ctx, LPaddle.current.x - 10, LPaddle.current.y);
 		drawPaddle(ctx, RPaddle.current.x, RPaddle.current.y);
 		drawBall(ctx, pos.current.x, pos.current.y);
@@ -164,16 +184,19 @@ function Pong() {
 	function isPaddleAtLevel(side, y) {
 		
 		let paddleY = (side == 1 ? RPaddle.current.y : LPaddle.current.y);
-		
+
+		// When at a paddle's x value, checks if the ball y value is inside the paddel's range to allow rebound
 		if (y > paddleY + 60 || y < paddleY - 60)
 			return (false);
 		return (true);
 	}
 
-	function getNewVector(side, y, oldVec) {
+	function getNewVector(side, y) {
 
 		let paddleY = (side == 1 ? RPaddle.current.y : LPaddle.current.y);
 		let newVec = 2 * (Math.abs(paddleY - y) / 60);
+
+		// A new vector is assigned relative to the location of a hit on a paddle
 		newVec = (newVec < 0.05 ? 0.05 : newVec);
 		newVec *= (paddleY - y > 0 ? -1 : 1);
 		return (newVec);
@@ -197,7 +220,7 @@ function Pong() {
 			// If this side's paddle is in range, the ball bounces off
 			if (isPaddleAtLevel(dir.current, pos.current.y) == true)
 			{
-				vec.current = getNewVector(dir.current, pos.current.y, vec.current);
+				vec.current = getNewVector(dir.current, pos.current.y);
 				dir.current *= -1;
 				newY = vec.current > 0 ? 491.1 : 9;
 				newX = dir.current * ((newY - obj.current.y) / vec.current) + obj.current.x;
@@ -221,7 +244,7 @@ function Pong() {
 					vec.current *= -1;
 			}
 		}
-		else // Otherwise, rebound didn't need any verification
+		else // Rebound didn't need any specific verification
 		{
 			if (newX >= 750 || newX <= 50) {
 				newX = (newX >= 750 ? 750 : 50);
@@ -234,8 +257,6 @@ function Pong() {
 
 		pos.current = obj.current;
 		obj.current = {x: newX, y: newY};
-		console.log("obj: ", obj.current);
-		console.log("dir: ", dir.current);
     };
 
 	useEffect(() => {
@@ -245,24 +266,25 @@ function Pong() {
 		const animate = (time) =>
 		{
 			if (time - lastUpdateTimeRef.current > 1000 / 61) {
+				// Calculating the distance from the current position to the target position
 				const dx = obj.current.x - pos.current.x;
 				const dy = obj.current.y - pos.current.y;
 				const distance = Math.sqrt(dx * dx + dy * dy);
 
+				// The game is more fun when you can move the paddles
 				handlePaddlesMovement();
 
-				if (distance <= speed.current) {
-					pos.current.x = obj.current.x; // Snap to target if close enough
+				if (distance <= speed.current) { // Snap to target if close enough, not going further than target
+					pos.current.x = obj.current.x;
 					pos.current.y = obj.current.y;
 					drawGame(context);
+					// Checking if the ball has scored a point yet
 					if (pos.current.x == 791.1 || pos.current.x == 9)
 						playAgain();
 					else if (gameStarted.current == true)
 						nextHit();
 				}
-				else
-				{
-					// Calculate movement step towards the target
+				else { // Determine the step's length towards the target, depending on speed
 					const angle = Math.atan2(dy, dx);
 					const newX = pos.current.x + Math.cos(angle) * speed.current;
 					const newY = pos.current.y + Math.sin(angle) * speed.current;
---
+++ README.md
@@ -0,0 +1,7 @@
+For the C screensaver (from inside the PongInC directory) :
+	- Command "make"
+	- Command "./pong"
+
+For the React Pong game (from inside the PongInReact directory) :
+	- Command "npm run dev"
+Then, in the browser of your choice, connect to "localhost:5173" url.
